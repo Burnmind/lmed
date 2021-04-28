8dd08f3496367b8d12e79e1d9180983ad881a87e
Alexander Ivlev <vealvy@yandex.ru>
2021-04-28 23:07:29 +0700

    * завел фиолетовую переменную + меню

--- header.php
+++ header.php
@@ -12,7 +12,44 @@
 </head>
 <body>
     <header>
+        <div class="container">
+            <div class="top-line">
 
+            </div>
+            <nav class="navbar navbar-expand-md">
+                <button class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#menuContent"
+                        aria-controls="menuContent"
+                        aria-expanded="false"
+                >
+
+                </button>
+                <div class="collapse navbar-collapse" id="menuContent">
+                    <ul class="navbar-nav mr-auto mb-2">
+                        <li class="nav-item">
+                            <a href="javascript:void(0)" class="nav-link">О клинике</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:void(0)" class="nav-link">Услуги стоматологии</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:void(0)" class="nav-link">Цены</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:void(0)" class="nav-link">Акции</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:void(0)" class="nav-link">Отзывы</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:void(0)" class="nav-link">Контакты</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </header>
     <main>
 
