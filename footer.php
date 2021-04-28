4e1e62cdd7020cccb9dcf9d51ab5656953804239
Alexander Ivlev <vealvy@yandex.ru>
2021-04-28 20:47:19 +0700

    * Стартовые файлы

---
+++ footer.php
@@ -0,0 +1,6 @@
+        </main>
+        <footer>
+            <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<? require 'header.php' ?>
+
+<? require 'footer.php' ?>
