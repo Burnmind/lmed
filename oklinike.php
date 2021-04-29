d0a22064623e9b339ba218fe5cb5b3822a26e85f
Haggar3037 <haggar3037@gmail.com>
2021-04-29 14:46:39 +0700

    косяк на косяке

--- oklinike.php
+++ oklinike.php
@@ -6,11 +6,34 @@
 	<div class="row">
 		<div class="col-sm">
 			<img src="photo.png" class="img-fluid">
+				<div class="row">
+					<div class="col-sm-4">
+						<img src="photo.png" class="img-fluid minipho mx-auto">
+					</div>
+					<div class="col-sm-4">
+						<img src="photo.png" class="img-fluid minipho mx-auto">
+					</div>
+					<div class="col-sm-4">
+						<img src="photo.png" class="img-fluid minipho mx-auto">
+					</div>
+					<div class="col-sm-4">
+						<img src="photo.png" class="img-fluid minipho mx-auto">
+					</div>
+					<div class="col-sm-4">
+						<img src="photo.png" class="img-fluid minipho mx-auto">
+					</div>
+					<div class="col-sm-4">
+						<img src="photo.png" class="img-fluid minipho mx-auto">
+					</div>
+			</div>
 		</div>
 		<div class="col-sm">
-			<h5><p>Наша клиника отличается особым отношением к своим пациентам. Прекрасное современное инновационное оснощение, уникальные передоыфе технологии, грамотные специалисты и уютная обстановка позволили нам завоевать признание и любовь наших пациентов. Мы в первую очередь заботимся о вашем здоровье. Стоматология для нас - это не только работа, но и признание.
-				<br><br>
-			Мы успешно осуществляем весь комплекс стоматологических услуг с максимальным комфортом для пациента</p></h5>
+			<h5><p>Наша клиника отличается особым отношением к&nbsp;своим пациентам. Прекрасное современное инновационное оснощение, уникальные передоыфе технологии, грамотные специалисты и&nbsp;уютная обстановка позволили нам завоевать признание и&nbsp;любовь наших пациентов. Мы&nbsp;в&nbsp;первую очередь заботимся о&nbsp;вашем здоровье. Стоматология для нас&nbsp;&mdash; это не&nbsp;только работа, но&nbsp;и&nbsp;признание.
+				<br><br><br>
+			Мы&nbsp;успешно осуществляем весь комплекс стоматологических услуг с&nbsp;максимальным комфортом для пациента
+				<br><br><br><br><br>
+			Мы&nbsp;используем только самое передовое оборудование
+			</p></h5>
 		</div>
 	</div>
 </div>
