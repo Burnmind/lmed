0c3b34b70c74d7e08c201fe1dde431a7282e00a6
Alexander Ivlev <vealvy@yandex.ru>
2021-04-29 13:04:50 +0700

    * ревью Хаджара

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
 <? require 'header.php' ?>
-<div class="container">
+<section class="about-page container">
 	<br><br><!-- ВСЕ ТОЖЕ САМОЕ -->
 	<br><br>
 	<h1><p  class="title">О КЛИНИКЕ</p><br><br></h1>
@@ -13,5 +13,5 @@
 			Мы успешно осуществляем весь комплекс стоматологических услуг с максимальным комфортом для пациента</p></h5>
 		</div>
 	</div>
-</div>
+</section>
 <? require 'footer.php' ?>
