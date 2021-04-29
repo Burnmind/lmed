df5b76ae3ce943f106b3914a4985971898e53e58
Alexander Ivlev <vealvy@yandex.ru>
2021-04-29 20:36:34 +0700

    * шапка

--- header.php
+++ header.php
@@ -13,21 +13,38 @@
 <body>
     <header>
         <div class="container">
-            <div class="top-line">
+            <div class="top-line d-flex">
+                <a href="/" class="logo-wrapper">
+                    <img src="public/img/logo.png" alt="Лмед">
+                    <span>Красивая улыбка - это простой способ быть лучше</span>
+                </a>
+                <div class="address-wrapper">
+                    <div class="address">
+                        <i class="icon-metro-pin"></i>
+                        <span>г.Москва, ул.Зюзинская, дом 6</span>
+                    </div>
+                    <div class="metro-list-wrapper">
+                        <i class="icon-metro"></i>
+                        <div class="metro-list d-flex flex-column align-items-center">
+                            <div>Новые Черемушки</div>
+                            <div>Профсоюзная</div>
+                            <div>Севастопольская</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="social-links-wrapper d-flex">
 
+                </div>
+                <div class="contact-block">
+                    <div class="phone">
+                        <i class="phone-icon"></i>
+                        <a href="tel:+74959688011">+7 (495) 968-80-11</a>
+                    </div>
+                </div>
             </div>
             <nav class="navbar navbar-expand-md">
-                <button class="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#menuContent"
-                        aria-controls="menuContent"
-                        aria-expanded="false"
-                >
-
-                </button>
                 <div class="collapse navbar-collapse" id="menuContent">
-                    <ul class="navbar-nav mr-auto mb-2">
+                    <ul class="navbar-nav mr-auto d-flex justify-content-between">
                         <li class="nav-item">
                             <a href="javascript:void(0)" class="nav-link">О клинике</a>
                         </li>
@@ -48,6 +65,15 @@
                         </li>
                     </ul>
                 </div>
+                <button class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#menuContent"
+                        aria-controls="menuContent"
+                        aria-expanded="false"
+                >
+
+                </button>
             </nav>
         </div>
     </header>
