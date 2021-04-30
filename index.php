ff1ccc2906b63a8b64c347e663107ebb2984c40a
Haggar3037 <haggar3037@gmail.com>
2021-05-01 01:28:20 +0700

    Что-то исправил, что-то хуже сделал

--- index.php
+++ index.php
@@ -5,51 +5,51 @@
 
             <div class="row">
                 <div class="col-sm-4">
-
-                    Лечение зубов<br><br>
-                    <img src="public/img/heal(1).png" class="img-fluid"><br>
-
+                    <p>Лечение зубов</p>
+                    <img src="public/img/heal(1).png" class="img-fluid">
                 </div>
                 <div class="col-sm-4">
-                    Имплантация зубов
+                    <p>Имплантация зубов</p>
                     <img src="public/img/implant(2).png" class="img-fluid">
                 </div>
                 <div class="col-sm-4">
-                    Отбеливание зубов
-                    <img src="public/img/white(3).png" class="img-fluid"><br>
-
+                    <p>Отбеливание зубов</p>
+                    <img src="public/img/white(3).png" class="img-fluid">
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-4">
+                    <p>Детская стоматология</p>
+                    <img src="public/img/child(4).png" class="img-fluid">
+                </div>
 
-                    Детская стоматология
-                    <img src="public/img/child(4).png" class="img-fluid"><br>
-                </div><br>
                 <div class="col-sm-4">
-                    Рентген-диагностика
-                    <img src="public/img/rent(5).png" class="img-fluid"><br>
-                </div><br>
+                    <p>Рентген-диагностика</p>
+                    <img src="public/img/rent(5).png" class="img-fluid">
+                </div>
+
                 <div class="col-sm-4">
-                    Ортодонтия<br><br>
-                    <img src="public/img/orto(6).png" class="img-fluid"><br>
-                </div><br>
+                    <p>Ортодонтия</p>
+                    <img src="public/img/orto(6).png" class="img-fluid">
+                </div>
 
-                <div class="row">
-                    <div class="col-sm-4">
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <p>Протезирование зубов</p>
+                    <img src="public/img/protes(7).png" class="img-fluid">
+                </div>
 
-                        Протезирование зубов
-                        <img src="public/img/protes(7).png" class="img-fluid"><br>
-                    </div><br>
-                    <div class="col-sm-4">
-                        Хирургическая стоматология
-                        <img src="public/img/hirurg(8).png" class="img-fluid"><br>
-                    </div><br>
-                    <div class="col-sm-4">
-                        Эстетическая стоматология
-                        <img src="public/img/estet(9).png" class="img-fluid" ><br>
+                <div class="col-sm-4">
+                    <p>Хирургическая стоматология</p>
+                    <img src="public/img/hirurg(8).png" class="img-fluid">
+                 </div>
 
-                    </div><br>
+                <div class="col-sm-4">
+                    <p>Эстетическая стоматология</p>
+                   <img src="public/img/estet(9).png" class="img-fluid" >
+                </div>
+
                 </div>
 
             </div>
