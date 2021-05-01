3f80bee0632c153969bc999b5407a5bc94207ef5
Alexander Ivlev <vealvy@yandex.ru>
2021-05-01 11:20:39 +0700

    * футер + футер адаптив

--- footer.php
+++ footer.php
@@ -1,5 +1,19 @@
         </main>
-        <footer>
+        <footer class="container">
+            <div class="footer-line d-flex justify-content-between">
+                <a href="/" class="logo-wrapper d-flex align-items-center">
+                    <img src="public/img/logo-white.png" alt="Лмед">
+                    <span class="d-none d-md-inline">Красивая улыбка - это простой способ быть лучше</span>
+                </a>
+                <div class="menu-wrapper d-flex justify-content-end align-items-center flex-wrap">
+                    <a href="javascript:void(0)" class="nav-link">Акции</a>
+                    <a href="javascript:void(0)" class="nav-link">Цены</a>
+                    <a href="javascript:void(0)" class="nav-link">Услуги стоматологии</a>
+                    <a href="javascript:void(0)" class="nav-link">О клинике</a>
+                    <a href="javascript:void(0)" class="nav-link">Отзывы</a>
+                    <a href="javascript:void(0)" class="nav-link">Контакты</a>
+                </div>
+            </div>
         </footer>
         <script src="node_modules/bootstrap/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
     </body>
