4630239b5aad737c0f57bc0a75f3f8381915a37d
Alexander Ivlev <vealvy@yandex.ru>
2021-05-16 20:33:16 +0700

    * фикс палладоса

--- service-detail.php
+++ service-detail.php
@@ -1,47 +1,85 @@
 <?  $pageType = 'service-detail'; ?>
 <? require 'header.php' ?>
 <section class="container">
-	 <div class="title1"><b>ХИРУРГИЧЕСКАЯ СТОМАТОЛОГИЯ</b></div>
+	 <h1 class="title1"><b>ХИРУРГИЧЕСКАЯ СТОМАТОЛОГИЯ</b></h1>
 	 <div class="row">
-	 	<div class="coll-md-6">
+	 	<div class="col-md-6">
 	 		<div class="roster">
-	 		<ul>
-	 		<li> <div class="separator"> </div><a href="" style="color:black" class="url">Удаление зубов на верхней челюсти </a></li> 
-	 		<li> <div class="separator"> </div><a href="" style="color:black" class="url">Удаление зубов на нижней челюсти </a></li> 
-	 		<li> <div class="separator"> </div><a href="" style="color:black" class="url">Сложное удаление </a></li> </a>
-	 		<li> <div class="separator"> </div><a href="" style="color:black" class="url">Удаление зубов беременным </a></li> 
-	 		<li> <div class="separator"> </div><a href="" style="color:black" class="url">Удаление зубов мудрости на верхней нижней челюсти </a></li> 
-	 		<li> <div class="separator"> </div><a href="" style="color:black" class="url">Удаление кисты </a></li> 
-	 		<li> <div class="separator"> </div><a href="" style="color:black" class="url">Удаление молочных зубов </a></li> 
-	 		<li> <div class="separator"> </div><a href="" style="color:black" class="url">Удаление ретинированного зуба </a></li> 
-	 		</ul></div>
-	 		</div>
-	 	</div>
-	 	<div class="coll-md-6">
-	 		<include>
-	 	</div>
+                <ul>
+                    <li>
+                        <div class="separator"></div>
+                        <a href="javascript:void(0)" class="url">
+                            Удаление зубов на верхней челюсти
+                        </a>
+                    </li>
+                    <li>
+                        <div class="separator"></div>
+                        <a href="javascript:void(0)" class="url">Удаление зубов на нижней челюсти</a>
+                    </li>
+                    <li>
+                        <div class="separator"> </div>
+                        <a href="javascript:void(0)" class="url">Сложное удаление</a>
+                    </li>
+                    <li>
+                        <div class="separator"> </div>
+                        <a href="javascript:void(0)" class="url">Удаление зубов беременным</a>
+                    </li>
+                    <li>
+                        <div class="separator"> </div>
+                        <a href="javascript:void(0)" class="url">Удаление зубов мудрости на верхней нижней челюсти </a>
+                    </li>
+                    <li>
+                        <div class="separator"> </div>
+                        <a href="javascript:void(0)" class="url">Удаление кисты </a>
+                    </li>
+                    <li>
+                        <div class="separator"> </div>
+                        <a href="javascript:void(0)" class="url">Удаление молочных зубов</a>
+                    </li>
+                    <li>
+                        <div class="separator"> </div>
+                        <a href="javascript:void(0)" class="url">
+                            Удаление ретинированного зуба
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+	 	<div class="col-md-6">
+            <? require 'partials/record-form.php' ?>
+        </div>
 	 </div>
-	<div class="image-wrapper">
-		<img src="public/img/teeth.png" alt="" class="img-fluid">
-	</div>
 	<div class="description-wrapper">
+        <div class="image-wrapper">
+            <img src="public/img/teeth.png" alt="" class="img-fluid">
+        </div>
 		<p><b> <span class="tittle"> ХИРУРГИЧЕСКАЯ СТОМАТОЛОГИЯ </span> </b> &nbsp;&mdash; это отрасль, направленная на&nbsp;устренение проблем полости рта, требующих постороннего вмешательства и&nbsp;применение специальных инструментов. Приоритетом специалистов данной области являются операции, направленные на&nbsp;сохраение целостности зубов. Но&nbsp;наиболее часто востребована <span class="color-text"><b>хирургическая стоматология для удаления зубов</b></span>. В&nbsp;некоторых случаях решение проблемы не&nbsp;представляется возможным без вмешательства стоматолога-хирурга.Участие специалиста необходимо, если: </p>
-		<p><div class="list">
-			<br><ul>
-			<li>Зуб травмирован (невозможно восстановить или сломался);</li>
-			<li>Сильно разрушенный зуб (невозможно восстановить);</li>
-			<li>Неэффективное консервативное лечение зуба, когда не&nbsp;наступает ремиссия и&nbsp;выздоровление;</li>
-			<li>Если классическое лечение корневых каналов невозможно;</li>
-			<li>На&nbsp;корне зуба киста, склонная к&nbsp;росту;</li>
-			<li>В&nbsp;челюсти киста от&nbsp;зачатка зуба, которая растет и&nbsp;повышает писк перелома челюсти;</li>
-			</ul></div>
-		 </p>
-		<p><span class="color-text"><b>Детская хиррургическая стоматология</b></span> бывает часто необходима, если у&nbsp;ребенка короткая уздечка языка или губы. Или начался воспальтельный процесс, требующий незамедлительного вмешательста.</p>
-		<p>Все операции проводятся нашими специалистами быстро, качественно и&nbsp;без применения дискомфорта пациенту. Перед проведением процедур в&nbsp;обязательном порядке проводится обследование проблемного участка и&nbsp;полноценная консультация специалиста. На&nbsp;протяжении всего срока лечения и&nbsp;после него, наблюдается состояние полости рта для предотвращения возникновения опалений. Современные технологии позволяют в&nbsp;нынешнее время проводить операции по&nbsp;восстановлению или удалению зуба в&nbsp;кратчайшие сроки без болевых ощущений. <span class="color-text"><b>Вы&nbsp;можете узнать цены хирургической стоматологии </b></span> и&nbsp;записаться на&nbsp;консультацию по&nbsp;номеру, указанному на&nbsp;сайте, или обратившись напрямую в&nbsp;офис <span class="color-text"><b>стоматологии детям и&nbsp;взрослым на&nbsp;Болотниковой.</b></span> </p>
+        <div class="list">
+            <br>
+            <ul>
+                <li>Зуб травмирован (невозможно восстановить или сломался);</li>
+                <li>Сильно разрушенный зуб (невозможно восстановить);</li>
+                <li>Неэффективное консервативное лечение зуба, когда не&nbsp;наступает ремиссия и&nbsp;выздоровление;</li>
+                <li>Если классическое лечение корневых каналов невозможно;</li>
+                <li>На&nbsp;корне зуба киста, склонная к&nbsp;росту;</li>
+                <li>В&nbsp;челюсти киста от&nbsp;зачатка зуба, которая растет и&nbsp;повышает писк перелома челюсти;</li>
+            </ul>
+        </div>
+        <p><span class="color-text"><b>Детская хиррургическая стоматология</b></span> бывает часто необходима, если у&nbsp;ребенка
+            короткая уздечка языка или губы. Или начался воспальтельный процесс, требующий незамедлительного
+            вмешательста.</p>
+        <p>Все операции проводятся нашими специалистами быстро, качественно и&nbsp;без применения дискомфорта пациенту.
+            Перед проведением процедур в&nbsp;обязательном порядке проводится обследование проблемного участка и&nbsp;полноценная
+            консультация специалиста. На&nbsp;протяжении всего срока лечения и&nbsp;после него, наблюдается состояние
+            полости рта для предотвращения возникновения опалений. Современные технологии позволяют в&nbsp;нынешнее
+            время проводить операции по&nbsp;восстановлению или удалению зуба в&nbsp;кратчайшие сроки без болевых
+            ощущений. <span class="color-text"><b>Вы&nbsp;можете узнать цены хирургической стоматологии </b></span> и&nbsp;записаться
+            на&nbsp;консультацию по&nbsp;номеру, указанному на&nbsp;сайте, или обратившись напрямую в&nbsp;офис
+            <span class="color-text"><b>стоматологии детям и&nbsp;взрослым на&nbsp;Болотниковой.</b></span></p>
 	</div>
 	 <div class="btn-wrapper d-flex justify-content-center">
-                <a href="#" type="button" class="btn outline-light button-primary-violet"> Записаться на прием</a>
-    </div><br>
+        <a href="#" type="button" class="btn outline-light button-primary-violet"> Записаться на прием</a>
+    </div>
 </section>
 
 <? require 'footer.php' ?>
